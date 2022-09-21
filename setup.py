44846b55b1924440f95077798611ff760527ae93
drorba <drorspace94@gmail.com>
2022-09-21 16:55:19 +0300

    rename libs and order setup.py

--- setup.py
+++ setup.py
@@ -1,27 +1,30 @@
-from setuptools import setup
+from setuptools import Extension, setup, find_packages
 
+requirements = open("requirements.txt").read().split()
 
 setup(
-    name='ambient_noise_py',
-    version=1,
-
-    author='Dror Bar',
-    author_email='dror.bar@weizmann.ac.il',
+    author="Dror Bar",
+    author_email="dror.bar@weizmann.ac.il",
     python_requires=">=3.7",
     classifiers=[
-        "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
         "Natural Language :: English",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "Topic :: Software Development :: Libraries",
         "Topic :: Scientific/Engineering :: Bio-Informatics",
         "Intended Audience :: Developers",
         "Intended Audience :: Science/Research",
     ],
-    description="Single-cell RNA Sequencing Analysis",
+    description="Single-cell RNA Sequencing ambient noise removal",
+    install_requires=requirements,
     license="MIT license",
-    url="https://github.com/tanaylab/ambient_noise_py.git",
-  )
+    include_package_data=True,
+    name="mcANR",
+    packages=find_packages(include=["mcANR"]),
+    url="https://github.com/tanaylab/mcANR.git",
+    version="0.1",
+)
