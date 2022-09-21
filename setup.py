6db76bf5539fca1b771f1c4f12dceb6bdae5113a
drorba <drorspace94@gmail.com>
2022-09-21 14:56:15 +0300

    working on package

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+
+setup(
+    name='ambient_noise_py',
+    version=1,
+
+    author='Dror Bar',
+    author_email='dror.bar@weizmann.ac.il',
+    python_requires=">=3.7",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+    ],
+    description="Single-cell RNA Sequencing Analysis",
+    license="MIT license",
+    url="https://github.com/tanaylab/ambient_noise_py.git",
+  )
